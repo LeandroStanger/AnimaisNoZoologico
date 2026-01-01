f76edda55a890816b13590f06d53bab50cef17fa
Leandro Stanger <leandrostanger@hotmail.com>
2026-01-01 18:42:36 -0300

    Updade to v2

--- script.js
+++ script.js
@@ -3,19 +3,27 @@ let animais = JSON.parse(localStorage.getItem('animais')) || [];
 document.getElementById("addBtn").addEventListener("click", function() {
     let nome = document.getElementById("nome").value;
     let idade = document.getElementById("idade").value;
+    let genero = document.getElementById("genero").value;
+    let especie = document.getElementById("especie").value;
+    let raca = document.getElementById("raca").value;
+    let ala = document.getElementById("ala").value;
 
 
-    if(nome === "" || idade === "") {
+    if(nome === "" || idade === "" || genero === "" || especie === "" || raca === "" || ala === "") {
         alert("Preencha os dois campos!");
         return;
     }
 
-    animais.push({nome: nome, idade: idade});
+    animais.push({nome: nome, idade: idade, genero: genero, especie: especie, raca: raca, ala: ala});
 
     localStorage.setItem("animais", JSON.stringify(animais));
 
     document.getElementById("nome").value = "";
     document.getElementById("idade").value = "";
+    document.getElementById("genero").value = "";
+    document.getElementById("especie").value = "";
+    document.getElementById("raca").value = "";
+    document.getElementById("ala").value = "";
     
     alert("Animal adicinado!");
 });
@@ -24,7 +32,7 @@ document.getElementById("mostrarBtn").addEventListener("click",
     function(){
         let texto = "Lista de animais:\n";
         animais.forEach(a => {
-            texto +=`Nome: ${a.nome} - Idade: ${a.idade} anos\n`;
+            texto +=`\nNome: ${a.nome} - Idade: ${a.idade} anos\nGenero: ${a.genero}\n Espécie: ${a.especie} - Raça: ${a.raca}\nAla do zoológico: ${a.ala}\n-------------------------------------------------------\n`;
         });
         document.getElementById("saida").textContent = texto;
     });
@@ -35,5 +43,9 @@ document.getElementById("mostrarBtn").addEventListener("click",
         document.getElementById("saida").textContent = "";
         document.getElementById("nome").value = "";
         document.getElementById("idade").value = "";
+        document.getElementById("genero").value = "";
+        document.getElementById("especie").value = "";
+        document.getElementById("raca").value = "";
+        document.getElementById("ala").value = "";
         alert(`Dados removidos!`)
     });
